b412403bc256ba434e01a3d93ea5ecc6e794548e
LiuZhaoning <1061357591@qq.com>
2023-05-05 19:48:58 +0800

    progress

---
+++ script/fisher_matrix/fisher_matrix_Deltaz1.8.sh
@@ -0,0 +1,111 @@
+#!/bin/bash  
+#PBS -N fisher_matrix
+#PBS -V 
+#PBS -lselect=1:ncpus=4:mem=5gb
+#PBS -j oe
+#PBS -k oe
+
+NUMCORE=4
+
+#z_r = 7.336, Delta_z = 1.8
+zeta=29.87
+Tvir=101500
+Rmfp=50
+SMOOTHING_SCALE=300
+FILE_NAME='fisher_matrix'
+
+python3 /home/liuzhaoning/antisym_observability/xi_A_HICO_unsmoothed.py $zeta $Tvir $Rmfp $SMOOTHING_SCALE $FILE_NAME $NUMCORE
+
+SMOOTHING_Pk=200
+python3 /home/liuzhaoning/antisym_observability/Pk_A_group_class.py $zeta $Tvir $Rmfp $SMOOTHING_SCALE $SMOOTHING_Pk $FILE_NAME $NUMCORE
+
+SMOOTHING_Pk=250
+python3 /home/liuzhaoning/antisym_observability/Pk_A_group_class.py $zeta $Tvir $Rmfp $SMOOTHING_SCALE $SMOOTHING_Pk $FILE_NAME $NUMCORE
+
+SMOOTHING_Pk=300
+python3 /home/liuzhaoning/antisym_observability/Pk_A_group_class.py $zeta $Tvir $Rmfp $SMOOTHING_SCALE $SMOOTHING_Pk $FILE_NAME $NUMCORE
+
+zeta=31.09
+Tvir=101500
+Rmfp=50
+SMOOTHING_SCALE=300
+FILE_NAME='fisher_matrix'
+
+python3 /home/liuzhaoning/antisym_observability/xi_A_HICO_unsmoothed.py $zeta $Tvir $Rmfp $SMOOTHING_SCALE $FILE_NAME $NUMCORE
+
+SMOOTHING_Pk=200
+python3 /home/liuzhaoning/antisym_observability/Pk_A_group_class.py $zeta $Tvir $Rmfp $SMOOTHING_SCALE $SMOOTHING_Pk $FILE_NAME $NUMCORE
+
+SMOOTHING_Pk=250
+python3 /home/liuzhaoning/antisym_observability/Pk_A_group_class.py $zeta $Tvir $Rmfp $SMOOTHING_SCALE $SMOOTHING_Pk $FILE_NAME $NUMCORE
+
+SMOOTHING_Pk=300
+python3 /home/liuzhaoning/antisym_observability/Pk_A_group_class.py $zeta $Tvir $Rmfp $SMOOTHING_SCALE $SMOOTHING_Pk $FILE_NAME $NUMCORE
+
+zeta=30.48
+Tvir=99460
+Rmfp=50
+SMOOTHING_SCALE=300
+FILE_NAME='fisher_matrix'
+
+python3 /home/liuzhaoning/antisym_observability/xi_A_HICO_unsmoothed.py $zeta $Tvir $Rmfp $SMOOTHING_SCALE $FILE_NAME $NUMCORE
+
+SMOOTHING_Pk=200
+python3 /home/liuzhaoning/antisym_observability/Pk_A_group_class.py $zeta $Tvir $Rmfp $SMOOTHING_SCALE $SMOOTHING_Pk $FILE_NAME $NUMCORE
+
+SMOOTHING_Pk=250
+python3 /home/liuzhaoning/antisym_observability/Pk_A_group_class.py $zeta $Tvir $Rmfp $SMOOTHING_SCALE $SMOOTHING_Pk $FILE_NAME $NUMCORE
+
+SMOOTHING_Pk=300
+python3 /home/liuzhaoning/antisym_observability/Pk_A_group_class.py $zeta $Tvir $Rmfp $SMOOTHING_SCALE $SMOOTHING_Pk $FILE_NAME $NUMCORE
+
+zeta=30.48
+Tvir=103500
+Rmfp=50
+SMOOTHING_SCALE=300
+FILE_NAME='fisher_matrix'
+
+python3 /home/liuzhaoning/antisym_observability/xi_A_HICO_unsmoothed.py $zeta $Tvir $Rmfp $SMOOTHING_SCALE $FILE_NAME $NUMCORE
+
+SMOOTHING_Pk=200
+python3 /home/liuzhaoning/antisym_observability/Pk_A_group_class.py $zeta $Tvir $Rmfp $SMOOTHING_SCALE $SMOOTHING_Pk $FILE_NAME $NUMCORE
+
+SMOOTHING_Pk=250
+python3 /home/liuzhaoning/antisym_observability/Pk_A_group_class.py $zeta $Tvir $Rmfp $SMOOTHING_SCALE $SMOOTHING_Pk $FILE_NAME $NUMCORE
+
+SMOOTHING_Pk=300
+python3 /home/liuzhaoning/antisym_observability/Pk_A_group_class.py $zeta $Tvir $Rmfp $SMOOTHING_SCALE $SMOOTHING_Pk $FILE_NAME $NUMCORE
+
+zeta=30.48
+Tvir=101500
+Rmfp=49
+SMOOTHING_SCALE=300
+FILE_NAME='fisher_matrix'
+
+python3 /home/liuzhaoning/antisym_observability/xi_A_HICO_unsmoothed.py $zeta $Tvir $Rmfp $SMOOTHING_SCALE $FILE_NAME $NUMCORE
+
+SMOOTHING_Pk=200
+python3 /home/liuzhaoning/antisym_observability/Pk_A_group_class.py $zeta $Tvir $Rmfp $SMOOTHING_SCALE $SMOOTHING_Pk $FILE_NAME $NUMCORE
+
+SMOOTHING_Pk=250
+python3 /home/liuzhaoning/antisym_observability/Pk_A_group_class.py $zeta $Tvir $Rmfp $SMOOTHING_SCALE $SMOOTHING_Pk $FILE_NAME $NUMCORE
+
+SMOOTHING_Pk=300
+python3 /home/liuzhaoning/antisym_observability/Pk_A_group_class.py $zeta $Tvir $Rmfp $SMOOTHING_SCALE $SMOOTHING_Pk $FILE_NAME $NUMCORE
+
+zeta=30.48
+Tvir=101500
+Rmfp=51
+SMOOTHING_SCALE=300
+FILE_NAME='fisher_matrix'
+
+python3 /home/liuzhaoning/antisym_observability/xi_A_HICO_unsmoothed.py $zeta $Tvir $Rmfp $SMOOTHING_SCALE $FILE_NAME $NUMCORE
+
+SMOOTHING_Pk=200
+python3 /home/liuzhaoning/antisym_observability/Pk_A_group_class.py $zeta $Tvir $Rmfp $SMOOTHING_SCALE $SMOOTHING_Pk $FILE_NAME $NUMCORE
+
+SMOOTHING_Pk=250
+python3 /home/liuzhaoning/antisym_observability/Pk_A_group_class.py $zeta $Tvir $Rmfp $SMOOTHING_SCALE $SMOOTHING_Pk $FILE_NAME $NUMCORE
+
+SMOOTHING_Pk=300
+python3 /home/liuzhaoning/antisym_observability/Pk_A_group_class.py $zeta $Tvir $Rmfp $SMOOTHING_SCALE $SMOOTHING_Pk $FILE_NAME $NUMCORE
---
+++ script/general_fisher_matrix/general_fisher_matrix_Deltaz1.8.sh
@@ -0,0 +1,45 @@
+#!/bin/bash  
+#PBS -N general_fisher_matrix
+#PBS -V 
+#PBS -lselect=1:ncpus=10:mem=5gb
+#PBS -j oe
+#PBS -k oe
+
+NUMCORE=10
+
+# z_r = 7.336, Delta_z = 1.77
+zeta=32.12
+Tvir=107700
+Rmfp=50
+SMOOTHING_SCALE=300
+FILE_NAME='general_fisher_matrix'
+
+python3 /home/liuzhaoning/antisym_observability/xi_A_HICO_unsmoothed.py $zeta $Tvir $Rmfp $SMOOTHING_SCALE $FILE_NAME $NUMCORE
+
+SMOOTHING_Pk=200
+python3 /home/liuzhaoning/antisym_observability/Pk_A_group_class.py $zeta $Tvir $Rmfp $SMOOTHING_SCALE $SMOOTHING_Pk $FILE_NAME $NUMCORE
+
+SMOOTHING_Pk=250
+python3 /home/liuzhaoning/antisym_observability/Pk_A_group_class.py $zeta $Tvir $Rmfp $SMOOTHING_SCALE $SMOOTHING_Pk $FILE_NAME $NUMCORE
+
+SMOOTHING_Pk=300
+python3 /home/liuzhaoning/antisym_observability/Pk_A_group_class.py $zeta $Tvir $Rmfp $SMOOTHING_SCALE $SMOOTHING_Pk $FILE_NAME $NUMCORE
+
+# z_r = 7.336, Delta_z = 1.83
+zeta=28.97
+Tvir=95670
+Rmfp=50
+SMOOTHING_SCALE=300
+FILE_NAME='general_fisher_matrix'
+
+python3 /home/liuzhaoning/antisym_observability/xi_A_HICO_unsmoothed.py $zeta $Tvir $Rmfp $SMOOTHING_SCALE $FILE_NAME $NUMCORE
+
+SMOOTHING_Pk=200
+python3 /home/liuzhaoning/antisym_observability/Pk_A_group_class.py $zeta $Tvir $Rmfp $SMOOTHING_SCALE $SMOOTHING_Pk $FILE_NAME $NUMCORE
+
+SMOOTHING_Pk=250
+python3 /home/liuzhaoning/antisym_observability/Pk_A_group_class.py $zeta $Tvir $Rmfp $SMOOTHING_SCALE $SMOOTHING_Pk $FILE_NAME $NUMCORE
+
+SMOOTHING_Pk=300
+python3 /home/liuzhaoning/antisym_observability/Pk_A_group_class.py $zeta $Tvir $Rmfp $SMOOTHING_SCALE $SMOOTHING_Pk $FILE_NAME $NUMCORE
+
