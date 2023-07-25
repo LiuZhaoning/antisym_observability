4cd7a45bce681fdd9273c4503ac5802a358ece93
LiuZhaoning <1061357591@qq.com>
2023-07-25 23:34:13 +0800

    progress

---
+++ script/paper2_group_class/Deltaz2.05_192.sh
@@ -0,0 +1,39 @@
+#!/bin/bash  
+#PBS -N 2_group
+#PBS -V 
+#PBS -lselect=1:ncpus=5:mem=5gb
+#PBS -j oe
+#PBS -k oe
+
+NUMCORE=5
+
+# z_r = 8
+zeta=25.7
+Tvir=52200
+Rmfp=50
+SMOOTHING_SCALE=384
+FILE_NAME='paper2_group_class'
+
+SMOOTHING_Pk=192
+python3 /home/liuzhaoning/antisym_observability/Pk_A_group_class.py $zeta $Tvir $Rmfp $SMOOTHING_SCALE $SMOOTHING_Pk $FILE_NAME $NUMCORE
+
+# z_r = 8.5
+zeta=30.3
+Tvir=45700
+Rmfp=50
+SMOOTHING_SCALE=384
+FILE_NAME='paper2_group_class'
+
+SMOOTHING_Pk=192
+python3 /home/liuzhaoning/antisym_observability/Pk_A_group_class.py $zeta $Tvir $Rmfp $SMOOTHING_SCALE $SMOOTHING_Pk $FILE_NAME $NUMCORE
+
+# z_r = 9
+zeta=35.5
+Tvir=40100
+Rmfp=50
+SMOOTHING_SCALE=384
+FILE_NAME='paper2_group_class'
+
+SMOOTHING_Pk=192
+python3 /home/liuzhaoning/antisym_observability/Pk_A_group_class.py $zeta $Tvir $Rmfp $SMOOTHING_SCALE $SMOOTHING_Pk $FILE_NAME $NUMCORE
+
