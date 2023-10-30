07a8c3a17ecb5a73bf1f83230f8faeedce416000
LiuZhaoning <1061357591@qq.com>
2023-10-30 10:50:16 +0800

    progress

---
+++ script/plot/paper2_group_class/fiducial_z8.5_duration2_DD0.34_Dz0.4_sup.sh
@@ -0,0 +1,48 @@
+#!/bin/bash  
+#PBS -N 2_group
+#PBS -V 
+#PBS -lselect=1:ncpus=10:mem=5gb
+#PBS -j oe
+#PBS -k oe
+
+NUMCORE=10
+
+
+# z_r = 8.5, Delta_z = 2
+zeta=32.71
+Tvir=50269
+Rmfp=50
+SMOOTHING_SCALE=384
+FILE_NAME='paper2_group_class'
+
+python3 /home/liuzhaoning/antisym_observability/xi_A_HICO_unsmoothed.py $zeta $Tvir $Rmfp $SMOOTHING_SCALE $FILE_NAME $NUMCORE
+
+SMOOTHING_Pk=192
+python3 /home/liuzhaoning/antisym_observability/Pk_A_group_class.py $zeta $Tvir $Rmfp $SMOOTHING_SCALE $SMOOTHING_Pk $FILE_NAME $NUMCORE
+
+SMOOTHING_Pk=288
+python3 /home/liuzhaoning/antisym_observability/Pk_A_group_class.py $zeta $Tvir $Rmfp $SMOOTHING_SCALE $SMOOTHING_Pk $FILE_NAME $NUMCORE
+
+SMOOTHING_Pk=384
+python3 /home/liuzhaoning/antisym_observability/Pk_A_group_class.py $zeta $Tvir $Rmfp $SMOOTHING_SCALE $SMOOTHING_Pk $FILE_NAME $NUMCORE
+
+
+
+# z_r = 8.9, Delta_z = 2
+zeta=37.27
+Tvir=45260
+Rmfp=50
+SMOOTHING_SCALE=384
+FILE_NAME='paper2_group_class'
+
+python3 /home/liuzhaoning/antisym_observability/xi_A_HICO_unsmoothed.py $zeta $Tvir $Rmfp $SMOOTHING_SCALE $FILE_NAME $NUMCORE
+
+SMOOTHING_Pk=192
+python3 /home/liuzhaoning/antisym_observability/Pk_A_group_class.py $zeta $Tvir $Rmfp $SMOOTHING_SCALE $SMOOTHING_Pk $FILE_NAME $NUMCORE
+
+SMOOTHING_Pk=288
+python3 /home/liuzhaoning/antisym_observability/Pk_A_group_class.py $zeta $Tvir $Rmfp $SMOOTHING_SCALE $SMOOTHING_Pk $FILE_NAME $NUMCORE
+
+SMOOTHING_Pk=384
+python3 /home/liuzhaoning/antisym_observability/Pk_A_group_class.py $zeta $Tvir $Rmfp $SMOOTHING_SCALE $SMOOTHING_Pk $FILE_NAME $NUMCORE
+
