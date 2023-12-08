60032e35c7658a22d15be40cec84ca5fbe7086ba
LiuZhaoning <1061357591@qq.com>
2023-12-09 00:20:03 +0800

    progress

---
+++ script/plot/paper2_group_class/fiducial_z8.5_duration2_DD0.34_Dz0.4_Pk.sh
@@ -0,0 +1,109 @@
+#!/bin/bash  
+#PBS -N 2_group
+#PBS -V 
+#PBS -lselect=1:ncpus=10:mem=5gb
+#PBS -j oe
+#PBS -k oe
+
+NUMCORE=10
+
+# z_r = 8.1, Delta_z = 1.6
+zeta=59.83
+Tvir=124089
+Rmfp=50
+SMOOTHING_SCALE=384
+FILE_NAME='paper2_group_class'
+
+
+SMOOTHING_Pk=96
+python3 /home/liuzhaoning/antisym_observability/Pk_A_group_class.py $zeta $Tvir $Rmfp $SMOOTHING_SCALE $SMOOTHING_Pk $FILE_NAME $NUMCORE
+
+# z_r = 8.1, Delta_z = 2
+zeta=28.68
+Tvir=55916.7
+Rmfp=50
+SMOOTHING_SCALE=384
+FILE_NAME='paper2_group_class'
+
+SMOOTHING_Pk=96
+python3 /home/liuzhaoning/antisym_observability/Pk_A_group_class.py $zeta $Tvir $Rmfp $SMOOTHING_SCALE $SMOOTHING_Pk $FILE_NAME $NUMCORE
+
+
+# z_r = 8.9, Delta_z = 2.4
+zeta=16.92
+Tvir=26738.3
+Rmfp=50
+SMOOTHING_SCALE=384
+FILE_NAME='paper2_group_class'
+
+SMOOTHING_Pk=96
+python3 /home/liuzhaoning/antisym_observability/Pk_A_group_class.py $zeta $Tvir $Rmfp $SMOOTHING_SCALE $SMOOTHING_Pk $FILE_NAME $NUMCORE
+
+
+#----------------------------------------------------------------
+
+# z_r = 8.5, Delta_z = 1.6
+zeta=69.73
+Tvir=111854
+Rmfp=50
+SMOOTHING_SCALE=384
+FILE_NAME='paper2_group_class'
+
+SMOOTHING_Pk=96
+python3 /home/liuzhaoning/antisym_observability/Pk_A_group_class.py $zeta $Tvir $Rmfp $SMOOTHING_SCALE $SMOOTHING_Pk $FILE_NAME $NUMCORE
+
+# z_r = 8.5, Delta_z = 2
+zeta=32.71
+Tvir= 50269
+Rmfp=50
+SMOOTHING_SCALE=384
+FILE_NAME='paper2_group_class'
+
+SMOOTHING_Pk=96
+python3 /home/liuzhaoning/antisym_observability/Pk_A_group_class.py $zeta $Tvir $Rmfp $SMOOTHING_SCALE $SMOOTHING_Pk $FILE_NAME $NUMCORE
+
+# z_r = 8.9, Delta_z = 2.4
+zeta=18.99
+Tvir=23967.5
+Rmfp=50
+SMOOTHING_SCALE=384
+FILE_NAME='paper2_group_class'
+
+SMOOTHING_Pk=96
+python3 /home/liuzhaoning/antisym_observability/Pk_A_group_class.py $zeta $Tvir $Rmfp $SMOOTHING_SCALE $SMOOTHING_Pk $FILE_NAME $NUMCORE
+
+#-----------------------------------------------
+
+# z_r = 8.9, Delta_z = 1.6
+zeta=81.12
+Tvir=100995
+Rmfp=50
+SMOOTHING_SCALE=384
+FILE_NAME='paper2_group_class'
+
+SMOOTHING_Pk=96
+python3 /home/liuzhaoning/antisym_observability/Pk_A_group_class.py $zeta $Tvir $Rmfp $SMOOTHING_SCALE $SMOOTHING_Pk $FILE_NAME $NUMCORE
+
+# z_r = 8.9, Delta_z = 2
+zeta=37.27
+Tvir= 45260
+Rmfp=50
+SMOOTHING_SCALE=384
+FILE_NAME='paper2_group_class'
+
+SMOOTHING_Pk=96
+python3 /home/liuzhaoning/antisym_observability/Pk_A_group_class.py $zeta $Tvir $Rmfp $SMOOTHING_SCALE $SMOOTHING_Pk $FILE_NAME $NUMCORE
+
+# z_r = 8.9, Delta_z = 2.4
+zeta=21.3
+Tvir=21514.2
+Rmfp=50
+SMOOTHING_SCALE=384
+FILE_NAME='paper2_group_class'
+
+SMOOTHING_Pk=96
+python3 /home/liuzhaoning/antisym_observability/Pk_A_group_class.py $zeta $Tvir $Rmfp $SMOOTHING_SCALE $SMOOTHING_Pk $FILE_NAME $NUMCORE
+
+#-----------------------------------------------
+
+
