f6c3e8d9f0a2150cef83c43bc90197764345975c
LiuZhaoning <1061357591@qq.com>
2023-12-13 23:24:49 +0800

    progress

---
+++ script/plot/paper3_f+l/fiducial_z7.336_duration1.8_DD0.37_Dz0.176.sh
@@ -0,0 +1,71 @@
+#!/bin/bash  
+#PBS -N 3_PMI
+#PBS -V 
+#PBS -lselect=1:ncpus=10:mem=5gb
+#PBS -j oe
+#PBS -k oe
+
+NUMCORE=10
+
+
+# z_r = 7.336, Delta_z = 1.8 - 0.37
+zeta=65.56
+Tvir=218000
+Rmfp=50
+SMOOTHING_SCALE=300
+FILE_NAME='fisher_matrix'
+
+python3 /home/liuzhaoning/antisym_observability/xi_A_HICO_unsmoothed.py $zeta $Tvir $Rmfp $SMOOTHING_SCALE $FILE_NAME $NUMCORE
+
+SMOOTHING_Pk=200
+python3 /home/liuzhaoning/antisym_observability/Pk_A_group_class.py $zeta $Tvir $Rmfp $SMOOTHING_SCALE $SMOOTHING_Pk $FILE_NAME $NUMCORE
+
+SMOOTHING_Pk=300
+python3 /home/liuzhaoning/antisym_observability/Pk_A_group_class.py $zeta $Tvir $Rmfp $SMOOTHING_SCALE $SMOOTHING_Pk $FILE_NAME $NUMCORE
+
+
+
+# z_r = 7.336, Delta_z = 1.8 + 0.37
+zeta=17.68
+Tvir=50160
+Rmfp=50
+SMOOTHING_SCALE=300
+FILE_NAME='fisher_matrix'
+
+python3 /home/liuzhaoning/antisym_observability/xi_A_HICO_unsmoothed.py $zeta $Tvir $Rmfp $SMOOTHING_SCALE $FILE_NAME $NUMCORE
+
+SMOOTHING_Pk=200
+python3 /home/liuzhaoning/antisym_observability/Pk_A_group_class.py $zeta $Tvir $Rmfp $SMOOTHING_SCALE $SMOOTHING_Pk $FILE_NAME $NUMCORE
+
+SMOOTHING_Pk=300
+python3 /home/liuzhaoning/antisym_observability/Pk_A_group_class.py $zeta $Tvir $Rmfp $SMOOTHING_SCALE $SMOOTHING_Pk $FILE_NAME $NUMCORE
+
+# z_r = 7.336 - 0.176, Delta_z = 1.8
+zeta=28.59
+Tvir=106500
+Rmfp=50
+SMOOTHING_SCALE=300
+FILE_NAME='fisher_matrix'
+
+python3 /home/liuzhaoning/antisym_observability/xi_A_HICO_unsmoothed.py $zeta $Tvir $Rmfp $SMOOTHING_SCALE $FILE_NAME $NUMCORE
+
+SMOOTHING_Pk=200
+python3 /home/liuzhaoning/antisym_observability/Pk_A_group_class.py $zeta $Tvir $Rmfp $SMOOTHING_SCALE $SMOOTHING_Pk $FILE_NAME $NUMCORE
+
+SMOOTHING_Pk=300
+python3 /home/liuzhaoning/antisym_observability/Pk_A_group_class.py $zeta $Tvir $Rmfp $SMOOTHING_SCALE $SMOOTHING_Pk $FILE_NAME $NUMCORE
+
+# z_r = 7.336 + 0.176, Delta_z = 1.8
+zeta=32.48
+Tvir=96730
+Rmfp=50
+SMOOTHING_SCALE=300
+FILE_NAME='fisher_matrix'
+
+python3 /home/liuzhaoning/antisym_observability/xi_A_HICO_unsmoothed.py $zeta $Tvir $Rmfp $SMOOTHING_SCALE $FILE_NAME $NUMCORE
+
+SMOOTHING_Pk=200
+python3 /home/liuzhaoning/antisym_observability/Pk_A_group_class.py $zeta $Tvir $Rmfp $SMOOTHING_SCALE $SMOOTHING_Pk $FILE_NAME $NUMCORE
+
+SMOOTHING_Pk=300
+python3 /home/liuzhaoning/antisym_observability/Pk_A_group_class.py $zeta $Tvir $Rmfp $SMOOTHING_SCALE $SMOOTHING_Pk $FILE_NAME $NUMCORE
